92c8d6882c210e2727cc3c83d98f78c0d0e1dd72
selmankara <selman@selmankara.com>
2021-02-07 14:31:26 +0300

    v.1

--- lib/main.dart
+++ lib/main.dart
@@ -23,57 +23,65 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FlatButton(
-              onPressed: () {
-                setState(() {
-                  seciliButon = 'SOL';
-                });
-                print('$seciliButon buton tıklandı');
-              },
-              child: Container(
-                width: 150,
-                height: 300,
-                color:
-                    seciliButon == 'SOL' ? Colors.blueAccent : Colors.blueGrey,
-                child: Center(
-                  child: Text(
-                    seciliButon == 'SOL' ? 'AKTİF' : 'PASİF',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    seciliButon = 'SOL';
+                  });
+                  print('$seciliButon buton tıklandı');
+                },
+                child: Container(
+                  width: 300,
+                  height: 150,
+                  color: seciliButon == 'SOL'
+                      ? Colors.blueAccent
+                      : Colors.blueGrey,
+                  child: Center(
+                    child: Text(
+                      seciliButon == 'SOL' ? 'AKTİF' : 'PASİF',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-            FlatButton(
-              onPressed: () {
-                setState(() {
-                  seciliButon = 'SAĞ';
-                });
-                print('$seciliButon buton tıklandı');
-              },
-              child: Container(
-                width: 150,
-                height: 300,
-                color:
-                    seciliButon == 'SAĞ' ? Colors.blueAccent : Colors.blueGrey,
-                child: Center(
-                  child: Text(
-                    seciliButon == 'SAĞ' ? 'AKTİF' : 'PASİF',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    seciliButon = 'SAĞ';
+                  });
+                  print('$seciliButon buton tıklandı');
+                },
+                child: Container(
+                  width: 300,
+                  height: 150,
+                  color: seciliButon == 'SAĞ'
+                      ? Colors.blueAccent
+                      : Colors.blueGrey,
+                  child: Center(
+                    child: Text(
+                      seciliButon == 'SAĞ' ? 'AKTİF' : 'PASİF',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
